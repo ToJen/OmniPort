9687c7a11947936c4dfaea2641e8e7ce2d35e20c
Tomisin <tomisinjenrola@gmail.com>
2017-12-24 18:53:46 -0330

    Added more features to profile and dashboard pages

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -1,19 +1,123 @@
 import React, { Component } from 'react'
 
+import gap from '../../img/gap.png'
+import hospital from '../../img/hospital.jpg'
+import mcdonalds from '../../img/mcdonalds.jpg'
+import university from '../../img/memorial-university.png'
+import bank from '../../img/standard-chartered.jpg'
+import gym from '../../img/gym.jpg'
+import sobeys from '../../img/sobeys.png'
+import dealer from '../../img/car-dealer.jpg'
+import cryptokitty from '../../img/cryptokitty.jpg'
+
+
+
+class ButtonController extends Component {
+  constructor(props) {
+    super(props)
+    this.handleRegisterClick = this.handleRegisterClick.bind(this)
+    this.handleRevokeClick = this.handleRevokeClick.bind(this)
+    this.handleEditClick = this.handleEditClick.bind(this)
+    this.state = {isRegistered: this.props.isRegistered}
+  }
+
+  handleRegisterClick() {
+    console.log('registered')
+    this.setState({isRegistered: true})
+  }
+
+  handleRevokeClick() {
+    console.log('revoked')
+    this.setState({isRegistered: false})
+  }
+
+  handleEditClick() {
+    console.log('edit')
+    // this.setState({isRegistered: false})
+  }
+
+  render() {
+    const isRegistered = this.state.isRegistered
+
+    let buttonDiv = null;
+    if (isRegistered) { 
+      buttonDiv = <div><button className="pure-button pure-button-primary info-control" onClick={this.handleEditClick}>Edit</button><button className="pure-button pure-button-primary info-control" onClick={this.handleRevokeClick}>Revoke</button></div>
+    } else {
+      buttonDiv = <button className="pure-button pure-button-primary info-control" onClick={this.handleRegisterClick}>Register</button>
+    }
+
+    return(
+      <div>
+        {buttonDiv}
+      </div>
+    )
+  }
+}
+
 class Dashboard extends Component {
   constructor(props, { authData }) {
     super(props)
     authData = this.props
   }
 
+
   render() {
+
+    let greetingMessage = null;
+    if(this.props.authData.name) {
+      greetingMessage = <p><strong>Congratulations {this.props.authData.name}!</strong> <br/> You've logged in with uPort successfully.</p>
+    } else {
+      greetingMessage = <p><strong>Congratulations!</strong> <br/> You've logged in with uPort successfully.</p>
+    }
+
     return(
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1">
             <br/><br/>
             <h1>Dashboard</h1>
-            <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with UPort successfully.</p>
+            {greetingMessage}
+          </div>
+        </div>
+        <hr/> <br/>
+        <h1>Entities</h1>
+        <p>These are places where you can register to get access to their services. You can restrict the amount of information each entity has or revoke their access to your information.</p>
+        <div className="pure-g">
+          <div className="pure-u-1-3">
+            <img src={gap} className="logos" alt="GAP store logo" /><br/>
+            <ButtonController isRegistered/>
+          </div>
+          <div className="pure-u-1-3">
+            <img src={hospital} className="logos" alt="New York Downtown Hospital logo" /><br/>
+            <ButtonController />
+          </div>
+          <div className="pure-u-1-3">
+            <img src={university} className="logos" alt="Memorial University logo" /><br/>
+            <ButtonController isRegistered/>
+          </div>
+          <div className="pure-u-1-3">
+            <img src={mcdonalds} className="logos" alt="McDonald's logo" /><br/>
+            <ButtonController isRegistered/>
+          </div>
+          <div className="pure-u-1-3">
+            <img src={bank} className="logos" alt="Standard Chartered Bank logo" /><br/>
+            <ButtonController />
+          </div>
+          <div className="pure-u-1-3">
+            <img src={gym} className="logos" alt="Gym Town logo" /><br/>
+            <ButtonController isRegistered/>
+          </div>
+          <div className="pure-u-1-3">
+            <img src={sobeys} className="logos" alt="Sobeys grocery store logo" /><br/>
+            <ButtonController isRegistered/>
+          </div>
+          <div className="pure-u-1-3">
+            <img src={dealer} className="logos" alt="Carworld car dealership logo" /><br/>
+            <ButtonController />
+          </div>
+          <div className="pure-u-1-3">
+            <img src={cryptokitty} className="logos" alt="Carworld car dealership logo" /><br/>
+            <ButtonController isRegistered/>
           </div>
         </div>
       </main>
--- src/user/layouts/profile/Profile.js
+++ src/user/layouts/profile/Profile.js
@@ -13,30 +13,31 @@ class Profile extends Component {
           <div className="pure-u-1-1">
             <br/><br/>
             <h1>My Profile</h1>
-            <p>Change these details in UPort to see them reflected here.</p>
-            <p>
-              <strong>Avatar</strong><br />
-              {this.props.authData.avatar}
-            </p>
+            <p>This page contains your general public information used for registration at places like stores, banks and more.
+            <br/>You can update your details in your uPort app to see them reflected here.</p>
+            <div>
+              <strong>Profile Picture</strong><br />
+              <img src={this.props.authData.avatar.uri} alt="profile" />
+            </div>
             <p>
               <strong>Name</strong><br />
-              {this.props.authData.name}
-            </p>
-            <p>
-              <strong>Phone</strong><br />
-              {this.props.authData.phone}
+              {this.props.authData.name.length > 0 ? this.props.authData.name : 'N/A'}
             </p>
             <p>
               <strong>Email</strong><br />
-              {this.props.authData.email}
+              {this.props.authData.email.length > 0 ? this.props.authData.email : 'N/A'}
+            </p>
+            <p>
+              <strong>Phone</strong><br />
+              {this.props.authData.phone.length > 0 ? this.props.authData.phone : 'N/A'}
             </p>
             <p>
               <strong>Location</strong><br />
-              {this.props.authData.country}
+              {this.props.authData.country.length > 0 ? this.props.authData.country : 'N/A'}
             </p>
             <p>
               <strong>Address</strong><br />
-              {this.props.authData.address}
+              {this.props.authData.address.length > 0 ? this.props.authData.address : 'N/A'}
             </p>
           </div>
         </div>
